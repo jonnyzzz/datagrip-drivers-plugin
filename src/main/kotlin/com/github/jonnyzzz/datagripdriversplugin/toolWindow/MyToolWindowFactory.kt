8d87f55a522d050d2becb50fa028fbaf207707c5
GitHub Action <action@github.com>
2023-04-19 15:39:58 +0000

    Template cleanup

--- src/main/kotlin/com/github/jonnyzzz/datagripdriversplugin/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/jonnyzzz/datagripdriversplugin/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.jonnyzzz.datagripdriversplugin.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.jonnyzzz.datagripdriversplugin.MyBundle
+import com.github.jonnyzzz.datagripdriversplugin.services.MyProjectService
 import javax.swing.JButton
 
 
